5d2f0cd7221c3775998e4785bc35f737042bf6ac
CapriceLoren <capriceloren@gmail.com>
2022-10-28 16:07:00 -0400

    update README

--- ghibli-u/src/Home.jsx
+++ ghibli-u/src/Home.jsx
@@ -2,8 +2,10 @@ function Home() {
 
     return (
       <div className="homeDiv">
-        <img src="https://thumbs.gfycat.com/FreePiercingHamster-size_restricted.gif" />
-        {/* <img src="https://cdn.dribbble.com/users/513906/screenshots/2859254/cut-2.gif" /> */}
+        <div className="homeImg">
+          <img src="https://thumbs.gfycat.com/FreePiercingHamster-size_restricted.gif" />
+          {/* <img src="https://cdn.dribbble.com/users/513906/screenshots/2859254/cut-2.gif" /> */}
+        </div>
         <p> Welcome Home!</p>
       </div> 
     );
